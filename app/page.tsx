148b5081ab644f60ce97f2e54a8332aa80b69620
Nick McCollum <mccollumn@webtrends.com>
2023-08-18 18:47:36 +0000

    Basic dashboard for homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,22 @@
+import { Grid } from "@mui/material";
+import Cases from "./cases/page";
+import Contacts from "./contacts/page";
+import Accounts from "./accounts/page";
+import Opportunities from "./opportunities/page";
+
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <>
+      <Grid container spacing={1}>
+        <Grid item xs={6}>
+          <Cases />
+          <Contacts />
+        </Grid>
+        <Grid item xs={6}>
+          <Accounts />
+          <Opportunities />
+        </Grid>
+      </Grid>
+    </>
+  );
 }
