0e335b21ed40a1912450745e6b7e8cf71dbf8e82
Nick McCollum <mccollumn@webtrends.com>
2023-09-06 15:40:14 +0000

    Updated setMenuOptions to receive dependency as string or array

--- app/forms/useForm.tsx
+++ app/forms/useForm.tsx
@@ -4,12 +4,25 @@ import { MenuItem } from "../types/types";
 export const useForm = ({ menuItems, initialMenuOptions }: UseFormProps) => {
   const [options, setOptions] = React.useState<any>(initialMenuOptions);
 
+  /**
+   * Sets the options for the provided menu name with the specified dependent value.
+   * The dependent value can be a string or array of strings.
+   */
   const setMenuOptions = React.useCallback(
-    (menu: string, dependantValue: string = "") => {
-      const optionObjs = menuItems.filter(
-        (item: MenuItem) =>
-          item.Menu_Name === menu && item.Menu_DependantValue === dependantValue
-      );
+    (menu: string, dependentValue: string = "") => {
+      const optionObjs = menuItems.filter((item: MenuItem) => {
+        if (Array.isArray(item.Menu_DependantValue)) {
+          return (
+            item.Menu_Name === menu &&
+            item.Menu_DependantValue.includes(dependentValue)
+          );
+        } else {
+          return (
+            item.Menu_Name === menu &&
+            item.Menu_DependantValue === dependentValue
+          );
+        }
+      });
       const optionsArray = optionObjs.map(
         (item: MenuItem) => item.Menu_Display
       );
@@ -21,6 +34,9 @@ export const useForm = ({ menuItems, initialMenuOptions }: UseFormProps) => {
     [menuItems]
   );
 
+  /**
+   * Sets the provided options for the specified menu.
+   */
   const setCustomMenuOptions = React.useCallback(
     (menu: string, optionsArray: any[]) => {
       setOptions((prev: any) => {
