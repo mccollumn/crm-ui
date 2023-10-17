6880f4ff696470a99e5d4498f07571adf3404475
Nick McCollum <mccollumn@webtrends.com>
2023-10-17 20:26:32 +0000

    Added theme provider

--- app/providers/providers.tsx
+++ app/providers/providers.tsx
@@ -1,7 +1,13 @@
 "use client";
 
+import theme from "@/theme";
+import { ThemeProvider } from "@mui/material/styles";
 import { SessionProvider } from "next-auth/react";
 
 export const Providers = ({ session, children }: any) => {
-  return <SessionProvider session={session}>{children}</SessionProvider>;
+  return (
+    <SessionProvider session={session}>
+      <ThemeProvider theme={theme}>{children}</ThemeProvider>
+    </SessionProvider>
+  );
 };
---
+++ theme.tsx
@@ -0,0 +1,13 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#264768",
+      light: "#417bb4",
+      dark: "#0a121b",
+    },
+  },
+});
+
+export default theme;
